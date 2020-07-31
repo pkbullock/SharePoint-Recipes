4199b42211f55210e0fb599798774c22daf4cbc9
Paul Bullock <paul.bullock@capacreative.co.uk>
2020-07-31 11:17:28 +0100

    Updates to script

--- Recipes/Site-Management/Get-LanguageLcids.ps1
+++ Recipes/Site-Management/Get-LanguageLcids.ps1
@@ -1,38 +1,35 @@
-# ----------------------------------------------------------------------------
-# Gets Language LCIDs
-#
-# Created:      Paul Bullock
-# Copyright (c) 2018 CaPa Creative Ltd
-# Date:         16/09/2018
-# License:      MIT License (MIT)
-# Disclaimer:   
+<# ----------------------------------------------------------------------------
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+Gets Language LCIDs
 
-# Useful reference: https://capa.ltd/pnp-posh-docs
+Created:      Paul Bullock
+Copyright (c) 2020 CaPa Creative Ltd
+Date:         16/09/2018
+License:      MIT License (MIT)
 
-# ----------------------------------------------------------------------------
+Disclaimer:   
 
-# PnP Online Connection
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Useful reference: https://capa.ltd/pnp-posh-docs
+
+---------------------------------------------------------------------------- #>
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
-    [string]$Url,
-    [switch]$RestorePermission
+    [string]$Url
 )
 begin{
 
-    # PnP Online Connection, check if already connecteds
-    if((Get-PnPConnection).Url -ne $Url){
-        Connect-PnPOnline -Url $Url -NoTelemetry
-    }
-
+    # PnP Online Connection, check if already connected
+    Connect-PnPOnline -Url $Url -NoTelemetry -UseWebLogin
 }
 process {
     
